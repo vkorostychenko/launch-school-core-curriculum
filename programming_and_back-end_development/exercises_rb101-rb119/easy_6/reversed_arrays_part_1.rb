5f4e708a33a3459fbb782591eef3a2e06f18eced
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-08-04 17:17:27 +0200

    Done exercise

---
+++ programming_and_back-end_development/exercises_rb101-rb119/easy_6/reversed_arrays_part_1.rb
@@ -0,0 +1,29 @@
+=begin
+Write a method that takes an Array as an argument, and reverses its elements in
+place; that is, mutate the Array passed into this method. The return value
+should be the same Array object.
+
+You may not use Array#reverse or Array#reverse!.
+=end
+
+def reverse!(arr)
+  arr.sort! {|a, b| arr.index(b) <=> arr.index(a)}
+end
+
+list = [1,2,3,4]
+result = reverse!(list)
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = %w(a b e d c)
+p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+p list == ["c", "d", "e", "b", "a"] # true
+
+list = ['abc']
+p reverse!(list) == ["abc"] # true
+p list == ["abc"] # true
+
+list = []
+p reverse!(list) == [] # true
+p list == [] # true

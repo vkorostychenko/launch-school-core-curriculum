3dadc045bd61a6f293f80f75c6f6f851f6bd5df2
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-08-14 11:48:54 +0200

    Done exercise

---
+++ programming_and_back-end_development/exercises_rb101-rb119/easy_9/counting_up.rb
@@ -0,0 +1,15 @@
+=begin
+Write a method that takes an integer argument, and returns an Array of all
+integers, in sequence, between 1 and the argument.
+
+You may assume that the argument will always be a valid integer that is greater
+than 0.
+=end
+
+def sequence(num)
+  1.upto(num).to_a
+end
+
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]

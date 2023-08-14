8b1efa5d80b7a0b1c5dd34c5a457f276d9e05077
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-08-14 18:08:22 +0200

    Done exercise

---
+++ programming_and_back-end_development/exercises_rb101-rb119/easy_9/sum_of_digits.rb
@@ -0,0 +1,22 @@
+=begin
+Write a method that takes one argument, a positive integer, and returns the sum
+of its digits.
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
+
+The tests above should print true.
+
+For a challenge, try writing this without any basic looping constructs
+(while, until, loop, and each).
+=end
+
+def sum(num)
+  num.digits.sum
+end
+
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45

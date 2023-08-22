a00dfde4ffce4c8871fe8a68cca9d4ce72ff929e
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-08-22 11:21:50 +0200

    Done exercise

--- programming_and_back-end_development/exercises_rb101-rb119/medium_1/word_to_digit.rb
+++ programming_and_back-end_development/exercises_rb101-rb119/medium_1/word_to_digit.rb
@@ -2,8 +2,6 @@
 Write a method that takes a sentence string as input, and returns the same
 string with any sequence of the words 'zero', 'one', 'two', 'three', 'four',
 'five', 'six', 'seven', 'eight', 'nine' converted to a string of digits.
-
-
 =end
 
 WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',

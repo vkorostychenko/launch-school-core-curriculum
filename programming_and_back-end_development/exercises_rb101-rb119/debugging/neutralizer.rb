ba459b063de07a1e4c15bbb9006ebd944af4d5e9
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-08-31 11:25:51 +0200

    Done exercise

---
+++ programming_and_back-end_development/exercises_rb101-rb119/debugging/neutralizer.rb
@@ -0,0 +1,46 @@
+=begin
+We wrote a neutralize method that removes negative words from sentences.
+However, it fails to remove all of them. What exactly happens?
+
+def neutralize(sentence)
+  words = sentence.split(' ')
+  words.each do |word|
+    words.delete(word) if negative?(word)
+  end
+
+  words.join(' ')
+end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts neutralize('These dull boring cards are part of a chaotic board game.')
+# Expected: These cards are part of a board game.
+# Actual: These boring cards are part of a board game.
+=end
+
+def neutralize(sentence)
+  words = sentence.split(' ')
+  # words.select! do |word|
+  #   !negative?(word)
+  # end
+  words.reject! { |word| negative?(word) }
+  words.join(' ')
+end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts neutralize('These dull boring cards are part of a chaotic board game.')
+# Expected: These cards are part of a board game.
+# Actual: These boring cards are part of a board game.

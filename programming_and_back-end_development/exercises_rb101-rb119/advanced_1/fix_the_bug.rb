b706a6c322aca7baeb1dc213f8e0bcfe8f03b6ff
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-09-05 19:06:31 +0200

    Done exercise

---
+++ programming_and_back-end_development/exercises_rb101-rb119/advanced_1/fix_the_bug.rb
@@ -0,0 +1,47 @@
+=begin
+The following code:
+
+def my_method(array)
+  if array.empty?
+    []
+  elsif
+    array.map do |value|
+      value * value
+    end
+  else
+    [7 * array.first]
+  end
+end
+
+p my_method([])
+p my_method([3])
+p my_method([3, 4])
+p my_method([5, 6, 7])
+
+is expected to print:
+
+[]
+[21]
+[9, 16]
+[25, 36, 49]
+
+However, it does not. Obviously, there is a bug. Find and fix the bug, then
+explain why the buggy program printed the results it did.
+=end
+
+def my_method(array)
+  if array.empty?
+    []
+  elsif array.size > 1
+    array.map do |value|
+      value * value
+    end
+  else
+    [7 * array.first]
+  end
+end
+
+p my_method([]) == []
+p my_method([3]) == [21]
+p my_method([3, 4]) == [9, 16]
+p my_method([5, 6, 7]) == [25, 36, 49]

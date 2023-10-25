0d1e0184a6ccf387fc2d74a986bd483dea11b348
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-10-25 15:10:04 +0200

    Exercise completed

--- programming_and_back-end_development/RB119/watch_others_code_problems/problem_7.rb
+++ programming_and_back-end_development/RB119/watch_others_code_problems/problem_7.rb
@@ -11,6 +11,30 @@ Only lower case letters will be used (a-z). No punctuation or digits will be
 included.
 =end
 
+=begin
+Input: two strings
+Output: a boolean
+
+Rules:
+  - return true if a portion of str1 characters can be rearranged to match str2
+  - consider only lowercase letters
+  - no punctuation or digits will be included
+
+Algorithm:
+  - iterate over str2
+    - count the current character in both str1 and str2
+    - return false if the number of characters in str1 is less than in str2
+  - return true
+=end
+
+def scramble(str1, str2)
+  str2.each_char do |char|
+    return false if str1.count(char) < str2.count(char)
+  end
+
+  true
+end
+
 p scramble('javaass', 'jjss') == false
 p scramble('rkqodlw', 'world') == true
 p scramble('cedewaraaossoqqyt', 'codewars') == true

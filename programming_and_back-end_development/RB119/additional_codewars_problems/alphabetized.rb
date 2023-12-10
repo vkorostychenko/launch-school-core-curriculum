5895654006cfb6d4ee0158a996d922489ae3a41f
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-10 17:42:40 +0100

    Refactored

--- programming_and_back-end_development/RB119/additional_codewars_problems/alphabetized.rb
+++ programming_and_back-end_development/RB119/additional_codewars_problems/alphabetized.rb
@@ -6,7 +6,7 @@ string in "case-insensitively-alphabetical-order-of-appearance" order.
 Whitespace and punctuation shall simply be removed!
 
 The input is restricted to contain no numerals and only words containing the 
-English alphabet letters.
+english alphabet letters.
 
 Example:
 
@@ -21,13 +21,21 @@ Rules:
 
 Algorithm:
   - given a string
-  - create an array of letters (a-z A-Z)
-  - sort the array of letters by their lowercase version
-  - join an array of letters together to a string and return it
+  - create a copy of a string and delete everything except a-z A-Z
+  - create an array of alphabet letters a-z
+  - iterate through the alphabet with the result string object
+    - create an array containing all instances of the current letter
+    - join the elements of the array together to a string
+    - append the string to the result
+  - return the result string
 =end
 
 def alphabetized(str)
-  str.scan(/[a-z]/i).sort_by(&:downcase).join
+  str = str.gsub(/[^a-z]/i, '')
+
+  ('a'..'z').each_with_object('') do |letter, result|
+    result << str.scan(/#{letter}/i).join
+  end
 end
 
 p alphabetized("") == ""
@@ -37,3 +45,6 @@ p alphabetized("a ") == "a"
 p alphabetized(" a ") == "a"
 p alphabetized("A b B a") == "AabB"
 p alphabetized(" a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z") == "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+p alphabetized("!@$%^&*()_+=-`,") == ""
+p alphabetized("The Holy Bible") == "BbeehHilloTy"
+p alphabetized("CodeWars can't Load Today") == "aaaaCcdddeLnooorstTWy"

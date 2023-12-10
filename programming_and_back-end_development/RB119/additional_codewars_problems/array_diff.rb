954e3639101e6753d6786ca7a2b2e28db6fe9a27
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-10 18:07:27 +0100

    Exrecise completed

---
+++ programming_and_back-end_development/RB119/additional_codewars_problems/array_diff.rb
@@ -0,0 +1,23 @@
+=begin
+Difficulty: ___
+
+Your goal in this kata is to implement a difference function, which subtracts 
+one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.
+
+array_diff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+=end
+
+def array_diff(a, b)
+  # a - b
+  a.difference(b)
+end
+
+p array_diff([1,2], [1]) == [2]
+p array_diff([1,2,2], [1]) == [2,2]
+p array_diff([1,2,2], [2]) == [1]
+p array_diff([1,2,2], []) == [1,2,2]
+p array_diff([], [1,2]) == []

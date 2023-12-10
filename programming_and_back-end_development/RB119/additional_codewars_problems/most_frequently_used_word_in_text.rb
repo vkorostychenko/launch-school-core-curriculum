e0c77d9ed91c0ce40498afd335b0365eed1b278d
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-10 14:56:26 +0100

    Exercise completed

---
+++ programming_and_back-end_development/RB119/additional_codewars_problems/most_frequently_used_word_in_text.rb
@@ -0,0 +1,84 @@
+=begin
+Difficulty: hard
+
+Write a function that, given a string of text (possibly with punctuation and 
+line-breaks), returns an array of the top-3 most occurring words, in descending
+ order of the number of occurrences.
+
+Assumptions:
+- A word is a string of letters (A to Z) optionally containing one or more 
+  apostrophes (') in ASCII. (No need to handle fancy punctuation.)
+- Matches should be case-insensitive, and the words in the result should be 
+  lowercased.
+- Ties may be broken arbitrarily.
+- If a text contains fewer than three unique words, then either the top-2 or 
+  top-1 words should be returned, or an empty array if a text contains no 
+  words.
+
+Examples:
+  top_3_words("In a village of La Mancha, the name of which I have no desire to call to
+  mind, there lived not long since one of those gentlemen that keep a lance
+  in the lance-rack, an old buckler, a lean hack, and a greyhound for
+  coursing. An olla of rather more beef than mutton, a salad on most
+  nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+  on Sundays, made away with three-quarters of his income.")
+  # => ["a", "of", "on"]
+
+  top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
+  # => ["e", "ddd", "aa"]
+
+  top_3_words("  //wont won't won't")
+  # => ["won't", "wont"]
+
+Bonus points (not really, but just for fun):
+- Avoid creating an array whose memory footprint is roughly as big as the input text.
+- Avoid sorting the entire array of unique words.
+
+Input: a string
+Output: an array
+
+Rules:
+  - given a string (text)
+    - letters (A to Z) optionally containing one or more 
+      apostrophes (') in ASCII
+    - Matches should be case-insensitive, and the words in the result should be
+      in lowercase
+    - Ties may be broken arbitrarily
+    - If a text contains fewer than three unique words, then either the top-2 or
+      top-1 words should be returned, or an empty array if a text contains no
+      words
+  - return an array containing the 3 topmost occurring words, in descending
+    order of the number of occurrences
+
+Algorithm:
+  - given a string
+  - create a downcased copy of a string
+  - delete everything that is NOT a space, a-z and a (')
+  - split the string into an array of words with spaces
+  - create a hash containing each element as a value and its count as a key from
+    the split array
+  - sort the hash in descending order by value
+  - get keys array from the hash
+  - return an array containing the first 3 elements from the array of keys
+=end
+
+def top_3_words(str)
+  copy_str = str.downcase.delete("^a-z '")
+  words = copy_str.split.select { |word| word.match?(/[a-z]/) }
+
+  words.tally.sort_by { |key, value| -value }.to_h.keys.take(3)
+end
+
+p top_3_words("a a a  b  c c  d d d d  e e e e e") == ["e", "d", "a"]
+p top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") == ["e", "ddd", "aa"]
+p top_3_words("  //wont won't won't ") == ["won't", "wont"]
+p top_3_words("  , e   .. ") == ["e"]
+p top_3_words("  ...  ") == []
+p top_3_words("  '  ") == []
+p top_3_words("  '''  ") == []
+p top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.""") == ["a", "of", "on"]

e0dfa2958efc59dd61927bc31ce484f94cbfb6c1
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-12 21:25:31 +0100

    Add: change_info

--- programming_and_back-end_development/RB120/lesson_1/oo_book/good_dog.rb
+++ programming_and_back-end_development/RB120/lesson_1/oo_book/good_dog.rb
@@ -1,22 +1,29 @@
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name, :height, :weight
 
-  def initialize(name)
-    @name = name
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
     "#{name} says arf!"
   end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
 end
 
-sparky = GoodDog.new('Sparky')
-fido = GoodDog.new('Fido')
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
 
-p sparky.speak
-p sparky.name
-sparky.name = 'Spartacus'
-p sparky.name
-
-p fido.speak
-p fido.name
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info

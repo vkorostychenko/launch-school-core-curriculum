45ea02d1b1be9244eda00f813effa5c8a5dd6e17
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-22 15:15:10 +0100

    Exercise completed

---
+++ programming_and_back-end_development/RB120/lesson_2/classes_and_objects/exercise_1.rb
@@ -0,0 +1,21 @@
+=begin 
+Given the below usage of the Person class, code the class definition.
+
+bob = Person.new('bob')
+bob.name                  # => 'bob'
+bob.name = 'Robert'
+bob.name                  # => 'Robert'
+=end
+
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new('bob')
+p bob.name                  # => 'bob'
+bob.name = 'Robert'
+p bob.name                  # => 'Robert'

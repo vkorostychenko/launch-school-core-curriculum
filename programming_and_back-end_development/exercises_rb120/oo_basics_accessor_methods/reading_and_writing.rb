865d3205dd66fe3b8969cdb56e78ebd1ac7adc12
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-28 17:28:50 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/oo_basics_accessor_methods/reading_and_writing.rb
@@ -0,0 +1,22 @@
+=begin
+Add the appropriate accessor methods to the following code.
+
+class Person
+end
+
+person1 = Person.new
+person1.name = 'Jessica'
+puts person1.name
+
+Expected output:
+
+Jessica
+=end
+
+class Person
+  attr_accessor :name
+end
+
+person1 = Person.new
+person1.name = 'Jessica'
+puts person1.name

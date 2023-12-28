6cebf73e661a26f3f58f085bb76e9a041ad592f4
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-28 17:59:38 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/oo_basics_accessor_methods/prefix_to_name.rb
@@ -0,0 +1,27 @@
+=begin
+Using the following code, add the appropriate accessor methods so that @name is
+returned with the added prefix 'Mr.'.
+
+class Person
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name
+
+Expected output:
+
+Mr. James
+=end
+
+class Person
+  attr_writer :name
+
+  def name
+    "Mr. #{@name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'James'
+puts person1.name

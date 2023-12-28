b76b7e94dcdee6b69e885d8d334b68499a24490b
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-28 16:38:43 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/oo_basics_inheritance/transportation.rb
@@ -0,0 +1,14 @@
+=begin
+Create a module named Transportation that contains three classes: Vehicle,
+Truck, and Car. Truck and Car should both inherit from Vehicle.
+=end
+
+module Transportation
+  class Vehicle
+  end
+
+  class Truck < Vehicle
+  end
+
+  class Car < Vehicle
+end

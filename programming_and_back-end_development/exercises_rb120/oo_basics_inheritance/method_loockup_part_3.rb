a2c6a2758c07c194921fad900d716021a2210985
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-28 16:35:29 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/oo_basics_inheritance/method_loockup_part_3.rb
@@ -0,0 +1,55 @@
+=begin
+Using the following code, determine the lookup path used when invoking
+cat1.color. Only list the classes and modules that Ruby will check when
+searching for the #color method.
+
+module Flyable
+  def fly
+    "I'm flying!"
+  end
+end
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+  include Flyable
+end
+
+bird1 = Bird.new('Red')
+bird1.color
+=end
+
+module Flyable
+  def fly
+    "I'm flying!"
+  end
+end
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+  include Flyable
+end
+
+bird1 = Bird.new('Red')
+bird1.color
+
+# Bird -> Flyable -> Animal -> Object -> Kernel -> BasicObject

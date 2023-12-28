6e5f848700aa9f8e87bb1745389df30ee06f6a68
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-28 16:33:10 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/oo_basics_inheritance/method_loockup_part_2.rb
@@ -0,0 +1,31 @@
+=begin
+Using the following code, determine the lookup path used when invoking
+cat1.color. Only list the classes and modules that Ruby will check when
+searching for the #color method.
+
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+=end
+
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+
+# Cat -> Animal -> Object -> Kernel -> BasicObject

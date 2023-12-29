ea8e7cd0f794378fe4ba71533626a340af2e45f5
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2023-12-29 16:51:21 +0100

    Exercise 'Banner Class' is completed.

---
+++ programming_and_back-end_development/exercises_rb120/easy_1/banner_class.rb
@@ -0,0 +1,88 @@
+=begin
+Behold this incomplete class for constructing boxed banners.
+
+class Banner
+  def initialize(message)
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+  end
+
+  def empty_line
+  end
+
+  def message_line
+    "| #{@message} |"
+  end
+end
+
+Complete this class so that the test cases shown below work as intended.
+You are free to add any methods or instance variables you need. However, do not
+make the implementation details public.
+
+You may assume that the input will always fit in your terminal window.
+
+Test Cases
+
+banner = Banner.new('To boldly go where no one has gone before.')
+puts banner
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+
+banner = Banner.new('')
+puts banner
++--+
+|  |
+|  |
+|  |
++--+
+=end
+
+class Banner
+  def initialize(message)
+    @message = message
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+    "+-#{'-' * @message.size}-+"
+  end
+  
+  def empty_line
+    "| #{' ' * @message.size} |"
+  end
+  
+  def message_line
+    "| #{@message} |"
+  end
+end
+
+banner = Banner.new('To boldly go where no one has gone before.')
+puts banner
+# +--------------------------------------------+
+# |                                            |
+# | To boldly go where no one has gone before. |
+# |                                            |
+# +--------------------------------------------+
+
+banner = Banner.new('')
+puts banner
+# +--+
+# |  |
+# |  |
+# |  |
+# +--+

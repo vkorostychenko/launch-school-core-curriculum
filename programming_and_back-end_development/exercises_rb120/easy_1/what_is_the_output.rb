0750789da7fefdfe4f8a729d3a7f16ccb0c0d76c
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-02 15:31:28 +0100

    Exercise 'What's the output?' is completed

---
+++ programming_and_back-end_development/exercises_rb120/easy_1/what_is_the_output.rb
@@ -0,0 +1,45 @@
+=begin
+Take a look at the following code:
+
+class Pet
+  attr_reader :name
+
+  def initialize(name)
+    @name = name.to_s
+  end
+
+  def to_s
+    @name.upcase!
+    "My name is #{@name}."
+  end
+end
+
+name = 'Fluffy'
+fluffy = Pet.new(name)
+puts fluffy.name
+puts fluffy
+puts fluffy.name
+puts name
+
+What output does this code print? Fix this class so that there are no surprises
+waiting in store for the unsuspecting developer.
+=end
+
+class Pet
+  attr_reader :name
+
+  def initialize(name)
+    @name = name.to_s
+  end
+
+  def to_s
+    "My name is #{@name.upcase}."
+  end
+end
+
+name = 'Fluffy'
+fluffy = Pet.new(name)
+puts fluffy.name
+puts fluffy
+puts fluffy.name
+puts name

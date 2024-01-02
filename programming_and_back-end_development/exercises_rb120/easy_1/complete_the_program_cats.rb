d850c411fe4a4a54635cb327007121394c47b6f2
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-02 16:26:14 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/easy_1/complete_the_program_cats.rb
@@ -0,0 +1,45 @@
+=begin
+Consider the following program.
+
+class Pet
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+class Cat < Pet
+end
+
+pudding = Cat.new('Pudding', 7, 'black and white')
+butterscotch = Cat.new('Butterscotch', 10, 'tan and white')
+puts pudding, butterscotch
+
+Update this code so that when you run it, you see the following output:
+
+My cat Pudding is 7 years old and has black and white fur.
+My cat Butterscotch is 10 years old and has tan and white fur.
+=end
+
+class Pet
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+class Cat < Pet
+  def initialize(name, age, colours)
+    super(name, age)
+    
+    @colours = colours
+  end
+
+  def to_s
+    "My cat #{@name} is #{@age} years old and has #{@colours} fur."
+  end
+end
+
+pudding = Cat.new('Pudding', 7, 'black and white')
+butterscotch = Cat.new('Butterscotch', 10, 'tan and white')
+puts pudding, butterscotch

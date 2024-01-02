5618e1c11c7951b5b81d7b03fc2d1d03dc81b227
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-02 18:44:14 +0100

    Exercise completed

---
+++ programming_and_back-end_development/exercises_rb120/easy_2/reverse_engineering.rb
@@ -0,0 +1,30 @@
+=begin
+Write a class that will display:
+
+ABC
+xyz
+
+when the following code is run:
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
+=end
+
+class Transform
+  def initialize(data)
+    @data = data
+  end
+
+  def uppercase
+    @data.upcase
+  end
+
+  def self.lowercase(str)
+    str.downcase
+  end
+end
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')

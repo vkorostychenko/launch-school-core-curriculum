078030cb75c4d43fd1b056ac52e94e699df8c6a8
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-09 17:44:18 +0100

    Add: play again

--- programming_and_back-end_development/RB120/lesson_2/rps/rock_paper_scissors.rb
+++ programming_and_back-end_development/RB120/lesson_2/rps/rock_paper_scissors.rb
@@ -64,11 +64,26 @@ class RPSGame
     end 
   end
 
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n."
+    end
+
+    answer == 'y'
+  end
+
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end

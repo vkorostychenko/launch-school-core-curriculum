4caf8caf017e11f7d7c6e57206695388312a4cc9
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-19 20:52:32 +0100

    Add: Board and Square classes

--- programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
+++ programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
@@ -1,12 +1,23 @@
 class Board
-  def initialize
+  INITIAL_MARKER = " "
 
+  def initialize
+    @squares = {}
+    (1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
+   end
+
+  def get_square_at(key)
+    @squares[key]
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker = marker
+  end
 
+  def to_s
+    @marker
   end
 end
 
@@ -25,6 +36,12 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
     puts ""
@@ -37,15 +54,18 @@ class TTTGame
   def display_board
     puts ""
     puts "     |     |"
-    puts "     |     |"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.
+      get_square_at(3)}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.
+      get_square_at(6)}"
     puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.
+      get_square_at(9)}"
     puts "     |     |"
     puts ""
   end

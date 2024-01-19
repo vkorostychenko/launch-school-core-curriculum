8b225ba16a227002aa6479ee78ff8748a23b6422
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-19 20:31:30 +0100

    Add: Display a board

--- programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
+++ programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
@@ -25,17 +25,43 @@ class Player
 end
 
 class TTTGame
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts ""
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
       
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+    # display_result
     display_goodbye_message
   end
 end

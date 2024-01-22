6885f796a78632dcdf394dea044eaa771b666a4c
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-01-22 15:52:16 +0100

    Add: Take turns

--- programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
+++ programming_and_back-end_development/RB120/lesson_5/tic_tac_toe.rb
@@ -5,15 +5,19 @@ class Board
 
   def initialize
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
-   end
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+  end
 
   def get_square_at(key)
     @squares[key]
   end
-  
+
   def set_square_at(key, marker)
     @squares[key].marker = marker
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 end
 
@@ -26,6 +30,10 @@ class Square
 
   def to_s
     @marker
+  end
+
+  def unmarked?
+    marker == Board::INITIAL_MARKER
   end
 end
 
@@ -61,51 +69,50 @@ class TTTGame
   def display_board
     puts ""
     puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.
-      get_square_at(3)}"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board
+      .get_square_at(3)}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.
-      get_square_at(6)}"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board
+      .get_square_at(6)}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.
-      get_square_at(9)}"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board
+      .get_square_at(9)}"
     puts "     |     |"
     puts ""
   end
 
   def human_moves
-    puts "Choose a square between 1-9: "
+    puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
     square = nil
     loop do
       square = gets.chomp.to_i
-      break if (1..9).include? square
+      break if board.unmarked_keys.include?(square)
       puts "Sorry, that's not a valid choice."
     end
 
     binding.pry
     board.set_square_at(square, human.marker)
   end
-  
+
   def computer_moves
-    board.set_square_at((1..9).to_a.sample, computer.marker)
+    board.set_square_at(board.unmarked_keys.sample, computer.marker)
   end
 
   def play
     display_welcome_message
+    display_board
+
     loop do
-      display_board
       human_moves
-      display_board
       # break if someone_won? || board_full?
-      
+
       computer_moves
       # break if someone_won? || board_full?
       display_board
-      break
     end
     # display_result
     display_goodbye_message

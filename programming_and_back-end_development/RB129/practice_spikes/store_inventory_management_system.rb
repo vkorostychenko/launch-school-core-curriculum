46bfb8ac3550d1dec76711656b544628511afde9
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-13 18:44:23 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/practice_spikes/store_inventory_management_system.rb
@@ -0,0 +1,18 @@
+
+=begin
+STORE INVENTORY MANAGEMENT SYSTEM
+
+You are tasked with implementing a program that simulates a basic inventory
+management system for a small store. The store has a limited number of items
+in stock, and the system should allow for adding items to the inventory,
+updating the stock levels, and retrieving the current stock levels for each
+item.
+
+Your program should have the following functionalities:
+Add an item to the inventory: Allow the user to input the name, price,
+and initial stock level for a new item and add it to the inventory. 
+Update stock levels: Allow the user to update the stock level of an existing
+item in the inventory by specifying the item name and the new stock level.
+Retrieve stock levels: Allow the user to retrieve the current stock level
+for a specific item in the inventory by specifying the item name.
+=end

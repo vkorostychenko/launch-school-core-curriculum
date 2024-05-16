6ab3d7239c949f88bbf94191ddb3728a81aab0ad
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-16 13:33:12 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/practice_spikes/restaurant.rb
@@ -0,0 +1,9 @@
+=begin
+RESTAURANT
+
+The restaurant has staff, and they all work and get paid
+The restaurant has servers who wear white shirts, serve orders and interact with customers
+Managers wear black shirts, can check inventory, serve orders and interact with customers
+Cooks can cook and check inventory, wear striped shirts
+Hosts seat guests, distribute menus and interact with customers
+=end

a87e1dc7984ac2e1ffbf7960cfc283b31d4bd576
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-16 13:31:57 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/practice_spikes/sports_team.rb
@@ -0,0 +1,11 @@
+=begin
+SPORTS TEAM
+
+Include 4 players (attacker, midfielder, defender, goalkeeper)
+All the players' jersey is blue, except the goalkeeper, his jersey is white with blue stripes
+All players can run and shoot the ball.
+Attacker should be able to lob the ball
+Midfielder should be able to pass the ball
+Defender should be able to block the ball
+The referee has a whistle. He wears black and is able to run and whistle.
+=end

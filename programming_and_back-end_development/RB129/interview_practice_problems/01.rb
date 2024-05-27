ce6a88502714b92b0ecc116cce23d5196f9805a5
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-27 20:17:56 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/interview_practice_problems/01.rb
@@ -0,0 +1,16 @@
+# Problem by Natalie Thompson
+class Person
+  def initialize(name, job)
+      @name = name
+  end 
+end
+
+roger = Person.new("Roger", "Carpenter")
+puts roger
+
+# Add 1 line of code for the person class
+# and 1 line of code in the initalize method. 
+# Other than that don't change Person.
+
+# Output:
+# "My name is Roger and I am a Carpenter"

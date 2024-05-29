b7e49a0c6afccf4bafe949f6d7691fc7f8550e8d
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-29 21:03:12 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/interview_practice_problems/05.rb
@@ -0,0 +1,15 @@
+# From Joseph Ochoa
+# give class Barbarian the functionality of the Monster instance attack method:
+  # If you used class inheritance, now try doing it without class inheritance directly.
+  # If you used modules, now try not using modules
+  # If you used duck typing, now don't use duck typing 
+
+  class Monster
+    def attack
+      "attacks!"
+    end
+  end
+
+  class Barbarian
+
+  end

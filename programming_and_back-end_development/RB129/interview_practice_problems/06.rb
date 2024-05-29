0b231d9d0be74ec3d9cee87adb45e584c2a1b112
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-29 21:25:00 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/interview_practice_problems/06.rb
@@ -0,0 +1,25 @@
+# Without adding any methods below, implement a solution; originally by Natalie Thompson, this version by Oscar Cortes
+
+class ClassA 
+  attr_reader :field1, :field2
+
+  def initialize(num)
+    @field1 = "xyz"
+    @field2 = num
+  end
+end
+
+class ClassB 
+  attr_reader :field1
+
+  def initialize
+    @field1 = "abc"
+  end
+end
+
+obj1 = ClassA.new(50)
+obj2 = ClassA.new(25)
+obj3 = ClassB.new
+
+p obj1 > obj2
+p obj2 > obj3

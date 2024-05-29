42cbd22b5e1432d8cab20e0b1c7cf3b0e433fdd0
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-29 20:23:59 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/interview_practice_problems/04.rb
@@ -0,0 +1,46 @@
+# Describe what the code below outputs and why from Joseph Ochoa
+
+module Attackable
+  def attack
+    "attacks!"
+  end
+end
+
+class Characters
+  attr_accessor :name, :characters 
+  
+  def initialize(name) 
+    #
+    self.name 
+    @characters = [] 
+  end
+  
+  def display
+    name
+  end
+  
+  protected 
+  attr_reader :name
+  attr_writer :name
+end
+
+class Monster < Characters
+  include Attackable
+  
+  def initialize(name)
+    super
+  end
+end
+
+class Barbarian < Characters
+  def ==(other)
+    if(super.self == super.self) # 
+      super.self == super.self
+    end
+  end
+end
+
+conan = Barbarian.new('barb') 
+rob = Monster.new('monst')
+conan.characters << rob
+p conan.display

8a0faebdbda7041b1dd75603305a99524e55fb67
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-29 21:01:00 +0200

    Add: solution

--- programming_and_back-end_development/RB129/interview_practice_problems/04.rb
+++ programming_and_back-end_development/RB129/interview_practice_problems/04.rb
@@ -7,19 +7,19 @@ module Attackable
 end
 
 class Characters
-  attr_accessor :name, :characters 
-  
-  def initialize(name) 
-    #
-    self.name 
-    @characters = [] 
+  attr_accessor :name, :characters
+
+  def initialize(name) # the parameter `name` isn't used within the method definition
+    self.name
+    @characters = []
   end
-  
+
   def display
-    name
+    name # calls the protected attr_reader `name` method
   end
-  
-  protected 
+
+  protected
+
   attr_reader :name
   attr_writer :name
 end
@@ -28,19 +28,26 @@ class Monster < Characters
   include Attackable
   
   def initialize(name)
-    super
+    super # invokes the method of the same name in the superclass while passing the parameter `name` as an argument
   end
 end
 
 class Barbarian < Characters
   def ==(other)
-    if(super.self == super.self) # 
+    if(super.self == super.self)
       super.self == super.self
     end
   end
 end
 
-conan = Barbarian.new('barb') 
+conan = Barbarian.new('barb')
 rob = Monster.new('monst')
 conan.characters << rob
 p conan.display
+
+=begin
+The code outputs `nil` and returns it, because within the definition of the
+instance method `#display` the protected method `name` is invoked, which in turn
+references the `@name` instance variable. Since the instance variable was not
+set anywhere in the code it references `nil`.
+=end

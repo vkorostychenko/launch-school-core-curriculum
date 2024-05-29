c92d1a1bbf06c04c8b65b412b9d21ba73b4e2fbb
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-29 21:21:58 +0200

    Add: solution

--- programming_and_back-end_development/RB129/interview_practice_problems/05.rb
+++ programming_and_back-end_development/RB129/interview_practice_problems/05.rb
@@ -4,6 +4,50 @@
   # If you used modules, now try not using modules
   # If you used duck typing, now don't use duck typing 
 
+  # class inheritance
+
+  # class Monster
+  #   def attack
+  #     "attacks!"
+  #   end
+  # end
+
+  # class Barbarian < Monster; end
+
+  # interface inheritance
+
+  # module Attackable
+  #   def attack
+  #     "attacks!"
+  #   end
+  # end
+
+  # class Monster
+  #   include Attackable
+  # end
+
+  # class Barbarian < Monster; end
+  
+  # p Barbarian.new.attack
+
+  # duck-typing
+
+#   class Monster
+#     def attack
+#       "attacks!"
+#     end
+#   end
+
+#   class Barbarian
+#     def attack
+#       "attacks!"
+#     end
+#   end
+
+# [Monster.new, Barbarian.new].each { |character| p character.attack }
+
+# directly
+
   class Monster
     def attack
       "attacks!"
@@ -11,5 +55,9 @@
   end
 
   class Barbarian
-
+    def attack
+      "attacks!"
+    end
   end
+
+p Monster.new.attack

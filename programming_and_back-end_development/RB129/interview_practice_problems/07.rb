5623cd031d0139db815e018fecd0236a313a95f3
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-05-30 11:26:42 +0200

    Add: solution

--- programming_and_back-end_development/RB129/interview_practice_problems/07.rb
+++ programming_and_back-end_development/RB129/interview_practice_problems/07.rb
@@ -1,23 +1,32 @@
 # Unknown
 
-class BenjaminButton 
-  
+class BenjaminButton
+  DIE_AGE = 100
+
+  attr_reader :actual_age, :appearance_age
+
   def initialize
+    @actual_age = 0
+    @appearance_age = DIE_AGE
   end
-  
+
+  def actual_age=(value)
+    @actual_age = value
+    @appearance_age = DIE_AGE - value
+  end
+
   def get_older
+    @actual_age += 1
+    @appearance_age -= 1
   end
-  
+
   def look_younger
   end
-  
+
   def die
+    self.actual_age = DIE_AGE
   end
 end
-
-class BenjaminButton
-end
-
 
 benjamin = BenjaminButton.new
 p benjamin.actual_age # => 0
@@ -28,7 +37,7 @@ p benjamin.actual_age
 
 benjamin.get_older
 p benjamin.actual_age # => 2
-p benjamin.appearance_age # => 99
+p benjamin.appearance_age # => 99 # Here, I'm assuming this was a typo, and the value should be 98
 
 benjamin.die
 p benjamin.actual_age # => 100

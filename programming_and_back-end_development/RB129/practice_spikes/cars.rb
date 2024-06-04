b59e7851910e978e3c99a3ef769c25e7b3c9f7d6
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-06-04 20:13:02 +0200

    Initial commit

---
+++ programming_and_back-end_development/RB129/practice_spikes/cars.rb
@@ -0,0 +1,17 @@
+=begin
+CARS
+
+There is a vehicle company called Cool Car Factory
+There are 3 trucks and two cars there:
+A blue garbage truck has orange wheels and picks up garbage
+An orange recycling truck has blue wheels and picks up recycling
+A purple car called Do Not Touch Anything Dirty has black wheels and if it touches something dirty says "hey don't touch any dirty stuff!"
+A green race car that races and has black wheels
+A red firetruck with black wheels that puts out fires
+
+All truck/car's of these types would have these same attributes and you can pass only one argument on instantiation
+All trucks can carry heavy stuff
+All cars and the firetruck can go fast
+All vehicles can drive
+Calling puts on an object of any class should return a sentence describing that object
+=end

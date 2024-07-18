f1272420597e47982265cd415fbd37f482f657c0
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-18 13:05:10 +0200

    Add: method each

--- programming_and_back-end_development/RB130-139/lesson_1/todo_list.rb
+++ programming_and_back-end_development/RB130-139/lesson_1/todo_list.rb
@@ -114,98 +114,23 @@ class TodoList
   def to_a
     @todos.clone
   end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
+    end
+  end
 end
 
-# given
 todo1 = Todo.new("Buy milk")
 todo2 = Todo.new("Clean room")
 todo3 = Todo.new("Go to gym")
+
 list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
 
-# ---- Adding to the list -----
-
-# add
-list.add(todo1)                 # adds todo1 to end of list, returns list
-list.add(todo2)                 # adds todo2 to end of list, returns list
-list.add(todo3)                 # adds todo3 to end of list, returns list
-
-begin
-  list.add(1)                     # raises TypeError with message "Can only add Todo objects"
-rescue TodoTypeError => e
-  puts e
+list.each do |todo|
+  puts todo                   # calls Todo#to_s
 end
-
-# <<
-# same behavior as add
-# list << todo1
-# list << todo2
-# list << todo3
-
-# ---- Interrogating the list -----
-
-# size
-p list.size                       # returns 3
-
-# first
-p list.first                      # returns todo1, which is the first item in the list
-
-# last
-p list.last                       # returns todo3, which is the last item in the list
-
-#to_a
-p list.to_a                      # returns an array of all items in the list
-
-#done?
-p list.done?                     # returns true if all todos in the list are done, otherwise false
-
-# ---- Retrieving an item in the list ----
-
-# item_at
-# list.item_at                    # raises ArgumentError
-p list.item_at(1)                 # returns 2nd item in list (zero based index)
-# list.item_at(100)               # raises IndexError
-
-# ---- Marking items in the list -----
-
-# mark_done_at
-# list.mark_done_at               # raises ArgumentError
-p list.mark_done_at(1)            # marks the 2nd item as done
-# list.mark_done_at(100)          # raises IndexError
-
-# mark_undone_at
-# list.mark_undone_at             # raises ArgumentError
-p list.mark_undone_at(1)          # marks the 2nd item as not done,
-# list.mark_undone_at(100)        # raises IndexError
-
-# done!
-p list.done!                      # marks all items as done
-
-# ---- Deleting from the list -----
-
-# shift
-# p list.shift                      # removes and returns the first item in list
-
-# pop
-# p list.pop                        # removes and returns the last item in list
-
-# remove_at
-# list.remove_at                  # raises ArgumentError
-p list.remove_at(1)               # removes and returns the 2nd item
-# list.remove_at(100)             # raises IndexError
-
-# ---- Outputting the list -----
-
-# to_s
-puts list.to_s                      # returns string representation of the list
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [ ] Clean room
-# [ ] Go to gym
-
-# or, if any todos are done
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [X] Clean room
-# [ ] Go to gym

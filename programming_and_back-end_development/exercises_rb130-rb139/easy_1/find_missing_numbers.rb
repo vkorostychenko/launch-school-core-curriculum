9c5d11fd387b745dff525cc8ca4e3006e2598ab6
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-28 06:55:42 +0200

    Add: the task

---
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/find_missing_numbers.rb
@@ -0,0 +1,10 @@
+# Write a method that takes a sorted array of integers as an argument, and returns
+# an array that includes all of the missing integers (in order) between the first
+# and last elements of the argument.
+
+# Examples:
+
+missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+missing([1, 2, 3, 4]) == []
+missing([1, 5]) == [2, 3, 4]
+missing([6]) == []

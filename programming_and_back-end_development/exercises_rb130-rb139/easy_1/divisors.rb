216f770d364b25a9ea19c300bdc2dc2db4116f19
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-28 07:29:52 +0200

    Add: the task

---
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/divisors.rb
@@ -0,0 +1,10 @@
+# Write a method that returns a list of all of the divisors of the positive integer
+# passed in as an argument. The return value can be in any sequence you wish.
+
+# Examples:
+
+divisors(1) == [1]
+divisors(7) == [1, 7]
+divisors(12) == [1, 2, 3, 4, 6, 12]
+divisors(98) == [1, 2, 7, 14, 49, 98]
+divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute

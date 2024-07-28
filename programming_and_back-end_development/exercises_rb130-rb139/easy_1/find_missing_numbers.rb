2958bc19ff9678e16eba69897c73a0db9a818e47
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-28 07:24:29 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/find_missing_numbers.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/find_missing_numbers.rb
@@ -4,7 +4,11 @@
 
 # Examples:
 
-missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
-missing([1, 2, 3, 4]) == []
-missing([1, 5]) == [2, 3, 4]
-missing([6]) == []
+def missing(arr)
+  (arr.first..arr.last).to_a - arr
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []

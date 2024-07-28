e63cd794dea3f1718ee8608eaf0dac07797f1b9c
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-28 07:38:26 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/divisors.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/divisors.rb
@@ -3,8 +3,12 @@
 
 # Examples:
 
-divisors(1) == [1]
-divisors(7) == [1, 7]
-divisors(12) == [1, 2, 3, 4, 6, 12]
-divisors(98) == [1, 2, 7, 14, 49, 98]
-divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
+def divisors(num)
+  (1..num).to_a.select { |div| num % div == 0 }
+end
+
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute

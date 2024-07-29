b050f78799ca036ba25b5f8d58225d15ae9d0745
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-29 07:19:15 +0200

    Add: the task

---
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/encrypted_pioneers.rb
@@ -0,0 +1,26 @@
+# The following list contains the names of individuals who are pioneers in the field
+# of computing or that have had a significant influence on the field. The names are
+# in an encrypted form, though, using a simple (and incredibly weak) form of
+# encryption called Rot13.
+
+# Nqn Ybirynpr
+# Tenpr Ubccre
+# Nqryr Tbyqfgvar
+# Nyna Ghevat
+# Puneyrf Onoontr
+# Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv
+# Wbua Ngnanfbss
+# Ybvf Unvog
+# Pynhqr Funaaba
+# Fgrir Wbof
+# Ovyy Tngrf
+# Gvz Orearef-Yrr
+# Fgrir Jbmavnx
+# Xbaenq Mhfr
+# Fve Nagbal Ubner
+# Zneiva Zvafxl
+# Lhxvuveb Zngfhzbgb
+# Unllvz Fybavzfxv
+# Tregehqr Oynapu
+
+# Write a program that deciphers and prints each of these names.

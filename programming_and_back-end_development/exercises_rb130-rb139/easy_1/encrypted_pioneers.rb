9f36a202710e0f83e46912ce60f61dffea3c05a3
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-31 18:19:39 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/encrypted_pioneers.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/encrypted_pioneers.rb
@@ -24,3 +24,47 @@
 # Tregehqr Oynapu
 
 # Write a program that deciphers and prints each of these names.
+
+ENCRYPTED_PIONEERS = [
+  'Nqn Ybirynpr',
+  'Tenpr Ubccre',
+  'Nqryr Tbyqfgvar',
+  'Nyna Ghevat',
+  'Puneyrf Onoontr',
+  'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv',
+  'Wbua Ngnanfbss',
+  'Ybvf Unvog',
+  'Pynhqr Funaaba',
+  'Fgrir Wbof',
+  'Ovyy Tngrf',
+  'Gvz Orearef-Yrr',
+  'Fgrir Jbmavnx',
+  'Xbaenq Mhfr',
+  'Fve Nagbal Ubner',
+  'Zneiva Zvafxl',
+  'Lhxvuveb Zngfhzbgb',
+  'Unllvz Fybavzfxv',
+  'Tregehqr Oynapu'
+].freeze
+
+ALPHABET = ('a'..'z').to_a
+
+def decipher(encrypted_char)
+  downcased_char = encrypted_char.downcase
+  return encrypted_char unless ALPHABET.include?(downcased_char)
+  
+  current_idx = ALPHABET.index(downcased_char)
+  new_idx = (current_idx - 13) % 26 # wrap around
+
+  downcased_char == encrypted_char ? ALPHABET[new_idx] : ALPHABET[new_idx].upcase
+end
+
+def rot13(encrypted_text)
+  encrypted_text.each_char.reduce('') do |result, encrypted_char|
+    result + decipher(encrypted_char)
+  end
+end
+
+ENCRYPTED_PIONEERS.each do |encrypted_name|
+  puts rot13(encrypted_name)
+end

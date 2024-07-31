73ce83b1c1b4765f18c142966a88e8857ff91f92
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-07-31 18:45:54 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_all.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_all.rb
@@ -23,10 +23,15 @@
 
 # Examples:
 
-all?([1, 3, 5, 6]) { |value| value.odd? } == false
-all?([1, 3, 5, 7]) { |value| value.odd? } == true
-all?([2, 4, 6, 8]) { |value| value.even? } == true
-all?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
-all?([1, 3, 5, 7]) { |value| true } == true
-all?([1, 3, 5, 7]) { |value| false } == false
-all?([]) { |value| false } == true
+def all?(collection)
+  collection.each { |element| return false unless yield(element) }
+  true
+end
+
+p all?([1, 3, 5, 6]) { |value| value.odd? } == false
+p all?([1, 3, 5, 7]) { |value| value.odd? } == true
+p all?([2, 4, 6, 8]) { |value| value.even? } == true
+p all?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p all?([1, 3, 5, 7]) { |value| true } == true
+p all?([1, 3, 5, 7]) { |value| false } == false
+p all?([]) { |value| false } == true

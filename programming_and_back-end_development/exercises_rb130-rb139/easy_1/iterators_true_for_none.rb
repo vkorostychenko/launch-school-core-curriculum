1fa25d1a0c705f17dba659dea16fdf6ff3c825bf
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-01 07:21:33 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_none.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_none.rb
@@ -24,10 +24,19 @@
 
 # Examples:
 
-none?([1, 3, 5, 6]) { |value| value.even? } == false
-none?([1, 3, 5, 7]) { |value| value.even? } == true
-none?([2, 4, 6, 8]) { |value| value.odd? } == true
-none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
-none?([1, 3, 5, 7]) { |value| true } == false
-none?([1, 3, 5, 7]) { |value| false } == true
-none?([]) { |value| true } == true
+def any?(collection)
+  collection.each { |element| return true if yield(element) }
+  false
+end
+
+def none?(collection, &block)
+  !any?(collection, &block)
+end
+
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true

0d16942ca12fb779c093dd3a88a28cbb46c31811
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-01 07:46:49 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_one.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_1/iterators_true_for_one.rb
@@ -23,10 +23,19 @@
 
 # Examples:
 
-one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
-one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
-one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
-one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
-one?([1, 3, 5, 7]) { |value| true }           # -> false
-one?([1, 3, 5, 7]) { |value| false }          # -> false
-one?([]) { |value| true }                     # -> false
+def one?(collection)
+  counter = 0
+  collection.each do |element|
+    counter += 1 if yield(element)
+    return false if counter > 1
+  end
+  counter == 1
+end
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    == true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     == false
+p one?([2, 4, 6, 8]) { |value| value.even? }    == false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p one?([1, 3, 5, 7]) { |value| true }           == false
+p one?([1, 3, 5, 7]) { |value| false }          == false
+p one?([]) { |value| true }                     == false

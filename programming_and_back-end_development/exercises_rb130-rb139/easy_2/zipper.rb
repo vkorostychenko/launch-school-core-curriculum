27d9bd86497a508861b4e4a9fdfefa1b476c23c0
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-01 21:18:08 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/zipper.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/zipper.rb
@@ -12,4 +12,10 @@
 
 # Example:
 
-zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
+def zip(array1, array2)
+  array1.each_with_index.with_object([]) do |(element, index), result|
+    result << [array1[index], array2[index]]
+  end
+end
+
+p zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]

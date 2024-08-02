e4b532dcb8d3dd6835c2c9e4128758f667a8f7d5
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-02 21:21:02 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_with_index.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_with_index.rb
@@ -13,6 +13,14 @@
 
 # Examples:
 
+def each_with_index(array)
+  index = 0
+  array.each do |element|
+    yield(element, index)
+    index += 1
+  end
+end
+
 result = each_with_index([1, 3, 6]) do |value, index|
   puts "#{index} -> #{value**index}"
 end

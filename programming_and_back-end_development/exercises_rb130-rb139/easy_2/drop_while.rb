2fe4a904203e216e38da0fb5c45c204a6733064b
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-02 21:07:47 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/drop_while.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/drop_while.rb
@@ -19,9 +19,16 @@
 
 # Examples:
 
-drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
-drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
-drop_while([1, 3, 5, 6]) { |value| true } == []
-drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
-drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
-drop_while([]) { |value| true } == []
+def drop_while(array)
+  array.each_with_index do |element, index|
+    return array[index..-1] unless yield(element)
+  end
+  []
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []

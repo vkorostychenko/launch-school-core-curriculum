01955c3cfd875f17ada682b5c74551316ee8123d
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-02 07:35:39 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/count.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/count.rb
@@ -19,9 +19,15 @@
 
 # Examples:
 
-count(1, 3, 6) { |value| value.odd? } == 2
-count(1, 3, 6) { |value| value.even? } == 1
-count(1, 3, 6) { |value| value > 6 } == 0
-count(1, 3, 6) { |value| true } == 3
-count() { |value| true } == 0
-count(1, 3, 6) { |value| value - 6 } == 3
+def count(*args)
+  args.reduce(0) do |total, element|
+    total + (yield(element) ? 1 : 0)
+  end
+end
+
+p count(1, 3, 6) { |value| value.odd? } == 2
+p count(1, 3, 6) { |value| value.even? } == 1
+p count(1, 3, 6) { |value| value > 6 } == 0
+p count(1, 3, 6) { |value| true } == 3
+p count() { |value| true } == 0
+p count(1, 3, 6) { |value| value - 6 } == 3

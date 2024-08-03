1716c8d97bfd871d4ffff037a67ffe650d276e24
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-03 07:03:21 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_with_object.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_with_object.rb
@@ -19,22 +19,27 @@
 
 # Examples:
 
+def each_with_object(array, object)
+  array.each { |element| yield(element, object) }
+  object
+end
+
 result = each_with_object([1, 3, 5], []) do |value, list|
   list << value**2
 end
-result == [1, 9, 25]
+p result == [1, 9, 25]
 
 result = each_with_object([1, 3, 5], []) do |value, list|
   list << (1..value).to_a
 end
-result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
 
 result = each_with_object([1, 3, 5], {}) do |value, hash|
   hash[value] = value**2
 end
-result == { 1 => 1, 3 => 9, 5 => 25 }
+p result == { 1 => 1, 3 => 9, 5 => 25 }
 
 result = each_with_object([], {}) do |value, hash|
   hash[value] = value * 2
 end
-result == {}
+p result == {}

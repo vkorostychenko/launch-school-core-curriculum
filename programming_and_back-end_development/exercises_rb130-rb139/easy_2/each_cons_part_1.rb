8062076aa0e9a1f85cb5dc448640aa7681785d77
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-05 07:36:55 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_cons_part_1.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_cons_part_1.rb
@@ -12,23 +12,32 @@
 
 # Examples:
 
+def each_cons(array)
+  counter = 0
+  while counter < array.size - 1 # stops iteration at the second to last index
+    yield(array[counter], array[counter + 1])
+    counter += 1
+  end
+  nil
+end
+
 hash = {}
 result = each_cons([1, 3, 6, 10]) do |value1, value2|
   hash[value1] = value2
 end
-result == nil
-hash == { 1 => 3, 3 => 6, 6 => 10 }
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
 
 hash = {}
 result = each_cons([]) do |value1, value2|
   hash[value1] = value2
 end
-hash == {}
-result == nil
+p hash == {}
+p result == nil
 
 hash = {}
 result = each_cons(['a', 'b']) do |value1, value2|
   hash[value1] = value2
 end
-hash == {'a' => 'b'}
-result == nil
+p hash == {'a' => 'b'}
+p result == nil

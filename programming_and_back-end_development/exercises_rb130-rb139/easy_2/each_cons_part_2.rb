af94c81b24cc0cf30e6cafc07406eca372257097
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-05 21:09:08 +0200

    Add: solution

--- programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_cons_part_2.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/easy_2/each_cons_part_2.rb
@@ -10,33 +10,41 @@
 # must not use any other methods that iterate through an Array or any other collection.
 
 # Examples:
+def each_cons(array, cons)
+  counter = 0
+  while counter <= array.size - cons
+    yield(*array.slice(counter, cons))
+    counter += 1
+  end
+  nil
+end
 
 hash = {}
 each_cons([1, 3, 6, 10], 1) do |value|
   hash[value] = true
 end
-hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
 
 hash = {}
 each_cons([1, 3, 6, 10], 2) do |value1, value2|
   hash[value1] = value2
 end
-hash == { 1 => 3, 3 => 6, 6 => 10 }
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
 
 hash = {}
 each_cons([1, 3, 6, 10], 3) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6], 3 => [6, 10] }
+p hash == { 1 => [3, 6], 3 => [6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 4) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6, 10] }
+p hash == { 1 => [3, 6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 5) do |value1, *values|
   hash[value1] = values
 end
-hash == {}
+p hash == {}

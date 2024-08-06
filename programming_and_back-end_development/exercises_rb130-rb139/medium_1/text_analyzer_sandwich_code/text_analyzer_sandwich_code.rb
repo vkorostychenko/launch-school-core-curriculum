1e9f1f1bc28ccb403eb56fa9826b0a9d895775a0
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-06 06:57:23 +0200

    Initial setup for exercise: RB130 > Medium 1 > Text Analyzer - Sandwich Code

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/text_analyzer_sandwich_code/text_analyzer_sandwich_code.rb
@@ -0,0 +1,34 @@
+# Fill out the rest of the Ruby code below so it prints output like that shown
+# in "Sample Output." You should read the text from a simple text file that you
+# provide. (We also supply some example text below, but try the program with your
+# text as well.)
+
+# This question requires that you read a simple text file. Try searching the Web
+# for information on how to do this, and also take a look at the File class in
+# the Ruby documentation. If you can't figure it out on your own, don't worry:
+# just put the data directly into your program; an Array with one element per
+# line would be ideal.
+
+# Read the text file in the #process method and pass the text to the block
+# provided in each call. Everything you need to work on is either part of the
+# #process method or part of the blocks. You need no other additions or changes.
+
+# The text you use does not have to produce the same numbers as the sample output
+# but should have the indicated format.
+
+class TextAnalyzer
+  def process
+    # your implementation
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { # your implementation }
+analyzer.process { # your implementation }
+analyzer.process { # your implementation }
+
+# Sample Output:
+
+# 3 paragraphs
+# 15 lines
+# 126 words

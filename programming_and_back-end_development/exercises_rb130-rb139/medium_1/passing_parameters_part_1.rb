c83034d80637ccf281204124aedd5049ef1da29a
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-07 07:46:11 +0200

    Initial setup for exercise: RB130 > Medium 1 > Passing Parameters Part 1

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/passing_parameters_part_1.rb
@@ -0,0 +1,10 @@
+# Modify the method below so that the display/output of items is moved to a block,
+# and its implementation is left up to the user of the gather method.
+
+items = ['apples', 'corn', 'cabbage', 'wheat']
+
+def gather(items)
+  puts "Let's start gathering food."
+  puts "#{items.join(', ')}"
+  puts "Nice selection of food we have gathered!"
+end

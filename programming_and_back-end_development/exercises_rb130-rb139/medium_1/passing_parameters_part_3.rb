dd79fbe9a05bd0441331cf99557a2b84d918f178
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-08 17:31:48 +0200

    Solution for exercise: RB130 > Medium 1 > Passing Parameters Part 3

--- programming_and_back-end_development/exercises_rb130-rb139/medium_1/passing_parameters_part_3.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/passing_parameters_part_3.rb
@@ -1,22 +1,22 @@
 # Given this code:
 
-# items = ['apples', 'corn', 'cabbage', 'wheat']
+items = ['apples', 'corn', 'cabbage', 'wheat']
 
-# def gather(items)
-#   puts "Let's start gathering food."
-#   yield(items)
-#   puts "We've finished gathering!"
-# end
+def gather(items)
+  puts "Let's start gathering food."
+  yield(items)
+  puts "We've finished gathering!"
+end
 
 # Fill out the following method calls for gather so that they produce the
 # corresponding output shown in numbers 1-4 listed below:
 
 # 1)
 
-# gather(items) do | , |
-#   puts
-#   puts
-# end
+gather(items) do |*items, last_item|
+  puts items.join(', ')
+  puts last_item
+end
 
 # Let's start gathering food.
 # apples, corn, cabbage
@@ -25,11 +25,11 @@
 
 # 2)
 
-# gather(items) do | , , |
-#   puts
-#   puts
-#   puts
-# end
+gather(items) do |first_item, *items, last_item|
+  puts first_item
+  puts items.join(', ')
+  puts last_item
+end
 
 # Let's start gathering food.
 # apples
@@ -39,10 +39,10 @@
 
 # 3)
 
-# gather(items) do | , |
-#   puts
-#   puts
-# end
+gather(items) do |first_item, *items|
+  puts first_item
+  puts items.join(', ')
+end
 
 # Let's start gathering food.
 # apples
@@ -51,9 +51,9 @@
 
 # 4)
 
-# gather(items) do | , , , |
-#   puts
-# end
+gather(items) do |first, second, third, fourth|
+  puts "#{first}, #{second}, #{third} and #{fourth}"
+end
 
 # Let's start gathering food.
 # apples, corn, cabbage, and wheat

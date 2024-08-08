2abf1391d3c567f6922d490cfa8bd16d23adcd76
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-08 19:43:24 +0200

    Initial setup for exercise: RB130 > Medium 1 > Method to Proc

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/method_to_proc.rb
@@ -0,0 +1,21 @@
+# Fill in the missing part of the following code so we can convert an array of
+# integers to base 8 (octal numbers). Use the comments for help in determining
+# where to make your modifications, and make sure to review the "Approach/Algorithm"
+# section for this exercise; it should prove useful.
+
+# Replace the two `method_name` placeholders with actual method calls
+def convert_to_base_8(n)
+  n.method_name.method_name
+end
+
+# Replace `argument` with the correct argument below
+# `method` is `Object#method`, not a placeholder
+base8_proc = method(argument).to_proc
+
+# We'll need a Proc object to make this code work
+# Replace `a_proc` with the correct object
+[8, 10, 12, 14, 16, 33].map(&a_proc)
+
+# The expected return value of map on this number array should be:
+
+# [10, 12, 14, 16, 20, 41]

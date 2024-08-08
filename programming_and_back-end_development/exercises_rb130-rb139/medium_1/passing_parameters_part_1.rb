307142d3f61ba1a1dc64b6ec26466ee0d585ef21
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-08 07:16:04 +0200

    Solution for exercise: RB130 > Medium 1 > Passing Parameters Part 1

--- programming_and_back-end_development/exercises_rb130-rb139/medium_1/passing_parameters_part_1.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/passing_parameters_part_1.rb
@@ -5,6 +5,8 @@ items = ['apples', 'corn', 'cabbage', 'wheat']
 
 def gather(items)
   puts "Let's start gathering food."
-  puts "#{items.join(', ')}"
+  yield(items)
   puts "Nice selection of food we have gathered!"
 end
+
+gather(items) { |produce| puts produce.join('; ')}

594f84f5e3b0df816432d329ff7ba31b75511d2b
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-08 20:43:27 +0200

    Solution for exercise: RB130 > Medium 1 > Method to Proc

--- programming_and_back-end_development/exercises_rb130-rb139/medium_1/method_to_proc.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/method_to_proc.rb
@@ -5,16 +5,16 @@
 
 # Replace the two `method_name` placeholders with actual method calls
 def convert_to_base_8(n)
-  n.method_name.method_name
+  n.to_s(8).to_i
 end
 
 # Replace `argument` with the correct argument below
 # `method` is `Object#method`, not a placeholder
-base8_proc = method(argument).to_proc
+base8_proc = method(:convert_to_base_8).to_proc
 
 # We'll need a Proc object to make this code work
 # Replace `a_proc` with the correct object
-[8, 10, 12, 14, 16, 33].map(&a_proc)
+p [8, 10, 12, 14, 16, 33].map(&base8_proc) == [10, 12, 14, 16, 20, 41]
 
 # The expected return value of map on this number array should be:
 

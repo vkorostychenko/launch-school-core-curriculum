a1fdc1e5f763085c13375eb1557a06b9476a9b6b
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-10 07:35:46 +0200

    Solution for exercise: RB130 > Medium 1 > Bubble Sort with Blocks

--- programming_and_back-end_development/exercises_rb130-rb139/medium_1/bubble_sort_with_blocks.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_1/bubble_sort_with_blocks.rb
@@ -6,7 +6,12 @@ def bubble_sort!(array)
   loop do
     swapped = false
     1.upto(array.size - 1) do |index|
-      next if array[index - 1] <= array[index]
+      if block_given?
+        next if yield(array[index - 1], array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
+
       array[index - 1], array[index] = array[index], array[index - 1]
       swapped = true
     end
@@ -31,24 +36,24 @@ end
 
 array = [5, 3]
 bubble_sort!(array)
-array == [3, 5]
+p array == [3, 5]
 
 array = [5, 3, 7]
 bubble_sort!(array) { |first, second| first >= second }
-array == [7, 5, 3]
+p array == [7, 5, 3]
 
 array = [6, 2, 7, 1, 4]
 bubble_sort!(array)
-array == [1, 2, 4, 6, 7]
+p array == [1, 2, 4, 6, 7]
 
 array = [6, 12, 27, 22, 14]
 bubble_sort!(array) { |first, second| (first % 7) <= (second % 7) }
-array == [14, 22, 12, 6, 27]
+p array == [14, 22, 12, 6, 27]
 
 array = %w(sue Pete alice Tyler rachel Kim bonnie)
 bubble_sort!(array)
-array == %w(Kim Pete Tyler alice bonnie rachel sue)
+p array == %w(Kim Pete Tyler alice bonnie rachel sue)
 
 array = %w(sue Pete alice Tyler rachel Kim bonnie)
 bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
-array == %w(alice bonnie Kim Pete rachel sue Tyler)
+p array == %w(alice bonnie Kim Pete rachel sue Tyler)

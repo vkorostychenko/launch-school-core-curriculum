14b6399f63e86e37665a1b3ac56e8657f40bf568
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-11 06:39:14 +0200

    Initial setup for exercise: RB130 > Advanced_1 > Internal vs External Iterators

---
+++ programming_and_back-end_development/exercises_rb130-rb139/advanced_1/internal_vs_external_iterators.rb
@@ -0,0 +1,10 @@
+# In this exercise, your task is to create a new Enumerator object.
+# When creating a new Enumerator, you may define what values are iterated upon.
+# We'll want to create an Enumerator that can iterate over an infinite list of
+# factorials. Once you make this new Enumerator, test it out by printing out the
+# first 7 factorial values, starting with zero factorial.
+
+# More specifically, print the first 7 factorials by using an "External Iterator".
+# Once you have done so, see what happens if you print 3 more factorials.
+# Now, reset your Enumerator (use the rewind method). Finally, print 7 more
+# factorials.

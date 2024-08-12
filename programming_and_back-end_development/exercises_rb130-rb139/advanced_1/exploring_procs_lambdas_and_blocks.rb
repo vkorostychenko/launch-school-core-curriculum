874bb87bfc04224bb63192d13fc46c5b722a9704
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-12 07:27:12 +0200

    Solution for exercise: RB130 > Advanced_1 > Exploring Procs, Lambdas, and Blocks: Definition and Arity

--- programming_and_back-end_development/exercises_rb130-rb139/advanced_1/exploring_procs_lambdas_and_blocks.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/advanced_1/exploring_procs_lambdas_and_blocks.rb
@@ -41,3 +41,42 @@ block_method_2('turtle') do |turtle, seal|
   puts "This is a #{turtle} and a #{seal}."
 end
 block_method_2('turtle') { puts "This is a #{animal}."}
+
+# Group 1:
+
+# A new Proc object can be created with a call of proc instead of Proc.new
+# A Proc is an object of class Proc
+# A Proc object does not require that the correct number of arguments are passed to it.
+# If nothing is passed, then nil is assigned to the block variable.
+
+# Group 2
+
+# A new Lambda object can be created with a call to lambda or ->.
+# We cannot create a new Lambda object with Lambda.new
+# A Lambda is actually a different variety of Proc.
+# While a Lambda is a Proc, it maintains a separate identity from a plain Proc.
+# This can be seen when displaying a Lambda: the string displayed contains an
+# extra "(lambda)" that is not present for regular Procs.
+# A lambda enforces the number of arguments. If the expected number of arguments
+# are not passed to it, then an error is thrown.
+
+# Group 3
+
+# A block passed to a method does not require the correct number of arguments.
+# If a block variable is defined, and no value is passed to it, then nil will be
+# assigned to that block variable.
+# If we have a yield and no block is passed, then an error is thrown.
+
+# Group 4
+
+# If we pass too few arguments to a block, then the remaining ones are assigned
+# a nil value.
+# Blocks will throw an error if a variable is referenced that doesn't exist in
+# the block's scope.
+
+# Comparison
+
+# Lambdas are types of Proc's. Technically they are both Proc objects.
+# An implicit block is a grouping of code, a type of closure, it is not an Object.
+# Lambdas enforce the number of arguments passed to them. Implicit blocks and
+# Procs do not enforce the number of arguments passed in.

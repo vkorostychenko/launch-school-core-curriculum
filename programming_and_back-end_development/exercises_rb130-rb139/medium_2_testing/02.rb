685e65b27d8b5d9f1e3371e6736bf661dcbb6356
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-12 15:25:10 +0200

    Initial setup for the set of exercises: RB130 > Medium 2: Testing

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/02.rb
@@ -0,0 +1,7 @@
+# Setup the Test Class - Cash Register
+
+# Let's start things from the ground up. We want to make a simple test suite for
+# our CashRegister class. Setup the initial testing file. You don't have to have
+# any tests in your test file. For this exercise, write everything you would need
+# to start testing CashRegister, excluding the tests themselves (necessary
+# requires, test class, etc.).

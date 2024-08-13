ae39dad571935c1c6d00055117da1c00601e6ae6
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-13 06:17:49 +0200

    Medium 2 > Testing: Setup the Test Class - Cash Register

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/cash_register_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < Minitest::Test
+end

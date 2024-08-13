44e91c371743698f00ad5a48e179538a4171f830
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-13 06:51:44 +0200

    Medium 2 > Testing: Test Accept Money Method - Cash Register

--- programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/cash_register_test.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/cash_register_test.rb
@@ -6,4 +6,14 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < Minitest::Test
+  def test_accept_money
+    register = CashRegister.new(1000)
+    transaction = Transaction.new(50)
+    transaction.amount_paid = 50
+
+    previous_amount = register.total_money
+    register.accept_money(transaction)
+    current_amount = register.total_money
+    assert_equal previous_amount + transaction.amount_paid, current_amount
+  end
 end

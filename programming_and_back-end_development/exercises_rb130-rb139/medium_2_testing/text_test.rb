28c8a2ba64e17fe50b2749b5ebb2c02b005a9eb5
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-15 17:13:14 +0200

    Medium 2 > Testing: Solution for exercise Test word_count method - Text

--- programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/text_test.rb
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/text_test.rb
@@ -24,6 +24,11 @@ class TextTest < Minitest::Test
     assert_equal expected_text, actual_text
   end
 
+  def test_word_count
+    text = Text.new(@file.read)
+    assert_equal 72, text.word_count
+  end
+
   def teardown
     @file.close
   end

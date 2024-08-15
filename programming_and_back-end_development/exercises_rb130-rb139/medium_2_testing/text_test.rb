f8c8a9c09bceaccb28c6325e2dc5d729b3e56d1c
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-08-15 17:00:49 +0200

    Medium 2 > Testing: Solution for exercise Test swap method - Text

---
+++ programming_and_back-end_development/exercises_rb130-rb139/medium_2_testing/text_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'text'
+
+class TextTest < Minitest::Test
+  def setup
+    @file = File.new('sample.txt', 'r')
+  end
+
+  def test_swap
+    text = Text.new(@file.read)
+    actual_text = text.swap('a', 'e')
+    expected_text = <<~TEXT.chomp
+    Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
+    Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
+    quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo
+    nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum
+    dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
+    et verius meuris, et pheretre mi.
+    TEXT
+
+    assert_equal expected_text, actual_text
+  end
+
+  def teardown
+    @file.close
+  end
+end

8dfb8c64b0386ded29a21bb724d7a6f1c60b2486
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-09-10 07:26:56 +0200

    Solution for the exercise: Ruby/JavaScript Challenges > Easy Challenges: Anagrams

--- programming_and_back-end_development/ruby_challenges/anagrams/anagrams.rb
+++ programming_and_back-end_development/ruby_challenges/anagrams/anagrams.rb
@@ -6,3 +6,25 @@
 # For example, given the word "listen" and a list of candidates like "enlists",
 # "google", "inlets", and "banana", the program should return a list containing "inlets".
 # Please read the test suite for the exact rules of anagrams.
+
+=begin
+Requirements:
+
+  - equal words are not anagrams
+  - case insensitive
+  - keep case when a word is selected
+
+=end
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    list.select do |anagram|
+      next if @word.downcase == anagram.downcase
+      @word.downcase.chars.sort == anagram.downcase.chars.sort
+    end
+  end
+end

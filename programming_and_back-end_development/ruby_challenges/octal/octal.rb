028edd9c8c8477f56e1c2f143d3b6c8b2c5cd7b8
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-09-12 19:25:46 +0200

    Initial setup for the exercise: Ruby/JavaScript Challenges > Easy Challenges: Octal

---
+++ programming_and_back-end_development/ruby_challenges/octal/octal.rb
@@ -0,0 +1,9 @@
+# Implement octal to decimal conversion.
+# Given an octal input string, your program should produce a decimal output.
+# Treat invalid input as octal 0.
+# The only valid digits in an octal number are 0, 1, 2, 3, 4, 5, 6, and 7.
+
+# Note: Implement the conversion yourself.
+# Don't use any built-in or library methods that perform the necessary conversions
+# for you. In this exercise, the code necessary to perform the conversion is what
+# we're looking for.

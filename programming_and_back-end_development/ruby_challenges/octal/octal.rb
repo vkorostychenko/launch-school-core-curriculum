8ae82cf884e0821aaf67a3760a59b58f4ffbe269
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-09-13 06:51:27 +0200

    Solution for the exercise: Ruby/JavaScript Challenges > Easy Challenges: Octal

--- programming_and_back-end_development/ruby_challenges/octal/octal.rb
+++ programming_and_back-end_development/ruby_challenges/octal/octal.rb
@@ -7,3 +7,27 @@
 # Don't use any built-in or library methods that perform the necessary conversions
 # for you. In this exercise, the code necessary to perform the conversion is what
 # we're looking for.
+
+# 233 # octal
+# = 2*8^2 + 3*8^1 + 3*8^0
+# = 2*64  + 3*8   + 3*1
+# = 128   + 24    + 3
+# = 155
+
+class Octal
+  attr_reader :number
+
+  def initialize(str)
+    @number = str
+  end
+
+  def to_decimal
+    return 0 unless number.match?(/\A[0-7]+\z/)
+
+    digits = number.to_i.digits
+    
+    digits.map.with_index do |digit, exponent|
+      digit*(8**exponent)
+    end.sum
+  end
+end

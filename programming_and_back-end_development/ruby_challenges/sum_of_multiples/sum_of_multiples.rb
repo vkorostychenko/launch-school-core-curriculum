c506cbf04f97ecbdb25dc8a06e7be06670814721
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-09-25 06:54:11 +0200

    Solution to the exercise: Ruby/JavaScript Challenges > Easy Challenges: Sum of Multiples

--- programming_and_back-end_development/ruby_challenges/sum_of_multiples/sum_of_multiples.rb
+++ programming_and_back-end_development/ruby_challenges/sum_of_multiples/sum_of_multiples.rb
@@ -6,3 +6,23 @@
 # For instance, if we list all the natural numbers up to, but not including,
 # 20 that are multiples of either 3 or 5, we get 3, 5, 6, 9, 10, 12, 15, and 18.
 # The sum of these multiples is 78.
+
+class SumOfMultiples
+  def self.to(number)
+    SumOfMultiples.new(3, 5).to(number)
+  end
+
+  def initialize(*numbers)
+    @set = numbers
+  end
+
+  def to(number)
+    multiples = []
+    (1...number).each do |potential_multiple|
+      @set.each do |number|
+        multiples << potential_multiple if potential_multiple % number == 0
+      end
+    end
+    multiples.uniq.sum
+  end
+end

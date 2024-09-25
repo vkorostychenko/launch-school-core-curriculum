4efdb3f1da3fa72c8766c1c68a47f133626ecb4b
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-09-25 15:19:38 +0200

    Initial setup for the exercise: Ruby/JavaScript Challenges > Easy Challenges: Series

---
+++ programming_and_back-end_development/ruby_challenges/series/series.rb
@@ -0,0 +1,15 @@
+# Write a program that will take a string of digits and return all the possible
+# consecutive number series of a specified length in that string.
+
+# For example, the string "01234" has the following 3-digit series:
+
+#     012
+#     123
+#     234
+
+# Likewise, here are the 4-digit series:
+
+#     0123
+#     1234
+
+# Finally, if you ask for a 6-digit series from a 5-digit string, you should throw an error.

44d2e32b2312b06dae85d46d875244ce4ec26880
Vladyslav Korostychenko <v.korostychenko@gmail.com>
2024-10-07 06:37:42 +0200

    Solution to the exercise: Ruby/JavaScript Challenges > Medium Challenges: Clock

--- programming_and_back-end_development/ruby_challenges/medium/clock/clock.rb
+++ programming_and_back-end_development/ruby_challenges/medium/clock/clock.rb
@@ -8,3 +8,40 @@
 
 # You may not use any built-in date or time functionality; just use arithmetic
 # operations.
+
+class Clock
+  MINUTES_A_DAY = 24 * 60
+
+  def self.at(hours, minutes=0)
+    new(hours, minutes)
+  end
+
+  def initialize(hours, minutes)
+    @time_in_minutes = ((hours * 60) + minutes) % MINUTES_A_DAY 
+  end
+
+  def -(min)
+    new_time = time_in_minutes - min
+    hours, minutes = new_time.divmod(60)
+    Clock.new(hours, minutes)
+  end
+  
+  def +(min)
+    new_time = time_in_minutes + min
+    hours, minutes = new_time.divmod(60)
+    Clock.new(hours, minutes)
+  end
+
+  def to_s
+    hours, minutes = time_in_minutes.divmod(60)
+    sprintf("%02d:%02d", hours, minutes)
+  end
+
+  def ==(other)
+    time_in_minutes == other.time_in_minutes
+  end
+
+  protected
+
+  attr_reader :time_in_minutes
+end
